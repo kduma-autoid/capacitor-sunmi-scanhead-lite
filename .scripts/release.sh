44f15d20ca8d0795a53b56d52d8febcbbb211ca3
Krystian Duma <git@krystian.duma.sh>
2023-10-04 15:45:36 +0200

    WIP CI

--- .scripts/release.sh
+++ .scripts/release.sh
@@ -7,7 +7,7 @@ dir=$( dirname -- "$( readlink -f -- "$0"; )"; )
 if [ -z "$(cd "$dir/../" && git status --porcelain)" ]
 then
   version=$(cd "$dir/../.scripts/" && ./bump_version.sh 2 VERSION) && echo "Releasing version $version" && \
-  cd "$dir/../example/" && npm install && npx cap sync android && npm run build:web && \
+  cd "$dir/../example/" && npm install && npm run build:android && \
   cd "$dir/../" && npm install && npm run verify && npm run build && \
   cd "$dir/../" && git add --all && git commit -m "Release $version" && git tag -a "v$version" -m "Release $version" && git push && git push --tags
 else
