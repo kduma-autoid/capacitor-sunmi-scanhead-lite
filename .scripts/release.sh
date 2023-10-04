ca30905be91f82f938418be79b7fe40f104188e6
Krystian Duma <git@krystian.duma.sh>
2023-10-04 14:55:35 +0200

    Release script

--- .scripts/release.sh
+++ .scripts/release.sh
@@ -6,9 +6,9 @@ dir=$( dirname -- "$( readlink -f -- "$0"; )"; )
 
 if [ -z "$(cd "$dir/../" && git status --porcelain)" ]
 then
-  version=$(cd "$dir/../.scripts/" && ./bump_version.sh 2 VERSION)
-  cd "$dir/../example/" && npm install && npx cap sync android && npm run build:web
-  cd "$dir/../" && npm install && npm run verify && npm run build
+  version=$(cd "$dir/../.scripts/" && ./bump_version.sh 2 VERSION) && echo "Releasing version $version" && \
+  cd "$dir/../example/" && npm install && npx cap sync android && npm run build:web && \
+  cd "$dir/../" && npm install && npm run verify && npm run build && \
   cd "$dir/../" && git add --all && git commit -m "Release $version" && git tag -a "v$version" -m "Release $version" && git push && git push --tags
 else
   echo "Working directory not clean. Please commit all changes before releasing."
